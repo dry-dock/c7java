b34601a99a2d7d73ce21e4155670c56911c8b74d
bindushree16 <37651858+bindushree16@users.noreply.github.com>
2019-06-11 21:15:37 +0530

    Update openjdk11.sh

--- version/openjdk11.sh
+++ version/openjdk11.sh
@@ -9,5 +9,5 @@ wget "https://download.java.net/java/GA/jdk11/9/GPL/openjdk-"$OPENJDK_VERSION"_l
 tar -xzf openjdk-"$OPENJDK_VERSION"_linux-x64_bin.tar.gz
 mv jdk-"$OPENJDK_VERSION"/ java-"$JAVA_VERSION"-openjdk-amd64
 
-sudo update-alternatives --install /usr/bin/java java /usr/lib/jvm/java-"$JAVA_VERSION"-openjdk-amd64/bin/java 1
-sudo update-alternatives --install /usr/bin/javac javac /usr/lib/jvm/java-"$JAVA_VERSION"-openjdk-amd64/bin/javac 1
+update-alternatives --install /usr/bin/java java /usr/lib/jvm/java-"$JAVA_VERSION"-openjdk-amd64/bin/java 1
+update-alternatives --install /usr/bin/javac javac /usr/lib/jvm/java-"$JAVA_VERSION"-openjdk-amd64/bin/javac 1
